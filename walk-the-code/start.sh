b7cb409b1b8313b60ae86b581652ff5006cb6ef6
Danilo Poccia <danilop@amazon.co.uk>
2026-04-14 16:29:59 +0100

    Update start.sh to use uvx (no pre-install required)

--- walk-the-code/start.sh
+++ walk-the-code/start.sh
@@ -2,6 +2,7 @@
 set -e
 PORT="${1:-8000}"
 CONFIG="$(cd "$(dirname "$0")" && pwd)/config.json"
+WTC='uvx --from "walk-the-code @ git+https://github.com/danilop/walk-the-code"'
 echo "Opening walkthrough at http://localhost:$PORT"
 (sleep 1 && python3 -m webbrowser "http://localhost:$PORT") &
-exec wtc-serve --config "$CONFIG" "$PORT"
+exec $WTC wtc-serve --config "$CONFIG" "$PORT"
